2a47f88c9fe8b853a18b391e4005a1cb30fcf696
Wuriqilang <543677991@qq.com>
2019-02-15 20:33:39 +0800

    image upload

--- controllers/cars.js
+++ controllers/cars.js
@@ -9,5 +9,31 @@ module.exports={
         ctx.render('cars-add.html',{
             title:'房车情报后台管理系统'
         })
+    },
+
+    'post /cars-titleImage':async (ctx,next)=>{
+        const multer = require('koa-multer');//加载koa-multer模块
+        //文件上传
+        //配置
+        var storage = multer.diskStorage({
+        //文件保存路径
+            destination: function (req, file, cb) {
+            cb(null, '/static/Details/')
+        },
+        //修改文件名称
+        filename: function (req, file, cb) {
+            var fileFormat = (file.originalname).split(".");
+            cb(null,Date.now() + "." + fileFormat[fileFormat.length - 1]);
+        }
+        })
+        //加载配置
+        var upload = multer({ storage: storage });
+        //路由
+        upload.single('file');
+
+            ctx.body = {
+                filename: ctx.req.file.filename//返回文件名
+            }
+        console.log(ctx.req.file.filename);
     }
 }
