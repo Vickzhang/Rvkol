37cffbd2b11fba8d4c037049b0d1ca8b689a26c1
Wuriqilang <543677991@qq.com>
2019-02-15 13:19:05 +0800

    车型库

--- controllers/cars.js
+++ controllers/cars.js
@@ -3,5 +3,11 @@ module.exports={
         ctx.render('cars-dashbord.html',{
             title:'房车情报后台管理系统'
         })
+    },
+
+    'GET /cars-add':async (ctx,next) => {
+        ctx.render('cars-add.html',{
+            title:'房车情报后台管理系统'
+        })
     }
 }
