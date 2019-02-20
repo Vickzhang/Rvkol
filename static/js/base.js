7852ffa75324c4ec5684e075c6ffd85fcab1ed71
Wuriqilang <543677991@qq.com>
2019-02-21 00:54:18 +0800

    主页修改

--- static/js/base.js
+++ static/js/base.js
@@ -1,6 +1,5 @@
-    var break_line=document.getElementById("text_desc_p");
-    var content=break_line.innerHTML;
-    break_line.innerHTML=content.replace(/。/g,"。<br>");
+var break_line = document.getElementById("text_desc_p");
+var content = break_line.innerHTML;
+break_line.innerHTML = content.replace(/。/g, "。<br>");
 
 
-    
