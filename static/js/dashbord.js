df6015ddd7b835862f71425d9a670828b1ca0676
Wuriqilang <543677991@qq.com>
2019-03-01 11:59:44 +0800

    测试

--- static/js/dashbord.js
+++ static/js/dashbord.js
@@ -31,6 +31,35 @@
 $(document).ready( function() {
     $("#summernote").summernote({
       minHeight: 300,             // set minimum height of editor
+      lang: 'zh-CN',
+      placeholder: '填写文章....',
+      toolbar: [
+        // <!--字体工具-->
+        ['fontname', ['fontname']], //字体系列                                 
+        ['style', ['bold', 'italic', 'underline', 'clear']], // 字体粗体、字体斜体、字体下划线、字体格式清除       
+        ['font', ['strikethrough', 'superscript', 'subscript']], //字体划线、字体上标、字体下标   
+        ['fontsize', ['fontsize']], //字体大小                                
+        ['color', ['color']], //字体颜色
+        
+        //<!--段落工具-->                
+        ['style', ['style']],//样式
+        ['para', ['ul', 'ol', 'paragraph']], //无序列表、有序列表、段落对齐方式
+        ['height', ['height']], //行高
+        
+        //<!--插入工具-->    
+        ['table',['table']], //插入表格    
+        ['hr',['hr']],//插入水平线                
+        ['link',['link']], //插入链接                
+        ['picture',['picture']], //插入图片                
+        ['video',['video']], //插入视频
+        
+        //<!--其它-->
+        ['fullscreen',['fullscreen']], //全屏
+        ['codeview',['codeview']], //查看html代码
+        ['undo',['undo']], //撤销
+        ['redo',['redo']], //取消撤销
+        ['help',['help']], //帮助
+      ],
       callbacks:{
         onImageUpload: function(file) { //图片默认以二进制的形式存储到数据库，调用此方法将请求后台将图片存储到服务器，返回图片请求地址到前端
             //将图片放入Formdate对象中      
