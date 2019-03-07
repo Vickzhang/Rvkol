f54111976ff22e1dfd260fc59d0d24b9fd29fea1
Wuriqilang <543677991@qq.com>
2019-03-07 13:55:03 +0800

    修改

--- app.js
+++ app.js
@@ -43,24 +43,24 @@ app.use(koaBody({
   formidable: {
     maxFileSize: 200*1024*1024, // 设置上传文件大小最大限制，默认2M
     maxFieldsSize:10*1024*104,
-    uploadDir: path.join(__dirname, './static/img'),
-      keepExtensions: true,
-      maxFieldsSize: 2 * 1024 * 1024,
-      onFileBegin:(name,file) => {
-        // 最终要保存到的文件夹目录
-        const dirName = getUploadDirName();
-        console.log(dirName);
-        const dir = path.join(__dirname, `/static/img/${dirName}`);
-        checkDirExist(dir);
-        // 获取文件名称
+    // uploadDir: path.join(__dirname, './static/img'),
+    //   keepExtensions: true,
+    //   maxFieldsSize: 2 * 1024 * 1024,
+    //   onFileBegin:(name,file) => {
+    //     // 最终要保存到的文件夹目录
+    //     const dirName = getUploadDirName();
+    //     console.log(dirName);
+    //     const dir = path.join(__dirname, `/static/img/${dirName}`);
+    //     checkDirExist(dir);
+    //     // 获取文件名称
 
-        // 重新覆盖 file.path 属性
-        file.path = `${dir}/${file.name}`;
-        console.log(file.path);
-        },
-        onError:(err)=>{
-          console.log(err);
-        }
+    //     // 重新覆盖 file.path 属性
+    //     file.path = `${dir}/${file.name}`;
+    //     console.log(file.path);
+    //     },
+    //     onError:(err)=>{
+    //       console.log(err);
+    //     }
   } 
     // multipart:true,
     // encoding:'gzip',
